43947b57fdf59aead1d71ee7300fa9e543c75462
nervar66 <zychu155@wp.pl>
2014-10-03 12:58:42 +0200

    Piotrek
    Bawię się tylko z apką od githuba do projektu

--- main.java
+++ main.java
@@ -1,6 +1,7 @@
 public class main {
     // komentarz
     public stati void main(String[] args) {
-	System.out.println("Hello, World!");
+	System.out.println("I am a man !");
+	//To tylko cwiczenia !
     }
 }
