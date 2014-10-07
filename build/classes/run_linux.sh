de0c8dc8414705a95a0393888638fcf6b0faeed7
dearn <dearn@dearn.pl>
2014-10-07 17:02:18 +0200

    JDBC przeniesiony do katalogu glownego

---
+++ build/classes/run_linux.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -cp .:/usr/share/jdbc-mysql/lib/jdbc-mysql.jar jdbc.jdbc_connection
--- main.java
+++
@@ -1,7 +0,0 @@
-public class main {
-    // komentarz
-    public stati void main(String[] args) {
-	System.out.println("I am a man !");
-	//To tylko cwiczenia !
-    }
-}
