1fa956d2f17277e7fe95526efe10c47dd216210c
nervar66 <zychu155@wp.pl>
2014-10-27 18:57:07 +0100

    Połączenie z bazą w visualu 2013 (Próby)
    Może tylko u mnie nie działa prosiłbym o sprawdzenie i wpisanie w patha
    w kodzie poprawnego jakby co.

---
+++ Proba/Proba/Index.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="Proba.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+         <asp:Label ID="Label3" runat="server" Text=""></asp:Label><br/>
+            <asp:Label ID="Label1" runat="server" Text="Login:"></asp:Label>
+        <asp:TextBox ID="Imie_Login" runat="server"></asp:TextBox>
+           
+            <br/>
+        <asp:Label ID="Label2" runat="server" Text="Hasło:"></asp:Label>
+        <asp:TextBox ID="Nazwisko_Haslo" runat="server"></asp:TextBox><br/>
+        <asp:Button ID="Button1" runat="server" Text="Zapisz" OnClick="Button1_Click" />
+     
+    </div>
+    </form>
+</body>
+</html>
---
+++ Proba/Proba/Index.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace Proba
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        int licznik=2;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack == true)
+            {
+                Label3.Text =( "Wczytano !");
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try {
+            SqlConnection conn = new SqlConnection("Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\\Users\\Samael\\Desktop\\Proba\\Proba\\App_Data\\Ludzie.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlCommand com = new SqlCommand("Insert into Table(Id,Imie,Nazwisko) Values (@Id,@Imie,@Nazwisko)",conn);
+            com.Parameters.AddWithValue("@Id",licznik);
+            com.Parameters.AddWithValue("@Imie", Imie_Login.Text);
+            com.Parameters.AddWithValue("@Nazwisko", Nazwisko_Haslo.Text);
+            conn.Open();
+            com.ExecuteNonQuery();
+            conn.Close();
+            licznik++;
+            }
+            catch (SqlException ex) { Label3.Text = ("Error :"+ex); }
+
+            if(IsPostBack)
+            {
+                Imie_Login.Text="";
+                Nazwisko_Haslo.Text="";
+            }
+        }
+    }
+}
