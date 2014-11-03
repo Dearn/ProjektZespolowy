1f71354300ccf44aa356d619b30b4ef00ae5f5cd
Piotr Filek <filek.piotr@gmail.com>
2014-11-03 15:41:03 +0100

    Revert "laczenie z baza danych przez web.config"
    This reverts commit 8be2414cf50bf8929720c305685cae1ea1007ca0.

--- WebApplication1/WebApplication1/Account/Login.aspx.cs
+++ WebApplication1/WebApplication1/Account/Login.aspx.cs
@@ -5,22 +5,19 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
-using System.Configuration;
 
 namespace WebApplication1.Account
 {
     public partial class Login : System.Web.UI.Page
     {
-        string connStr;
         protected void Page_Load(object sender, EventArgs e)
         {
-     
+
         }
 
         protected void Zaloguj_Click(object sender, EventArgs e)
         {
-            connStr = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
-            SqlConnection conn = new SqlConnection(connStr);
+            SqlConnection conn = new SqlConnection("Data Source=.\\JAJA;AttachDbFilename=C:\\Users\\Samael\\Desktop\\ProjektZespolowy\\WebApplication1\\WebApplication1\\App_Data\\Database1.mdf;Integrated Security=True;User Instance=True;");
             conn.Open();
             string checkuser = "select count(*) from Uzytkownicy where imie='" + TextBox_login + "'";
             SqlCommand com = new SqlCommand(checkuser, conn);
